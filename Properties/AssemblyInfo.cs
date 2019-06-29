b65dd56e537cbad32314b299441a4a925e293ceb
Joseph Merhej <49675896+JosephMerhej@users.noreply.github.com>
2019-06-29 08:05:49 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataImport++")]
+[assembly: AssemblyTitle("Data Import++")]
 [assembly: AssemblyDescription("Create, Update, Upsert, Delete records in CRM by importing an excel file and defining KEY columns.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Joseph Merhej")]
-[assembly: AssemblyProduct("DataImport++")]
+[assembly: AssemblyProduct("Data Import++")]
 [assembly: AssemblyCopyright("Copyright ©  2019 Joseph Merhej")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2019.2.37")]
-[assembly: AssemblyFileVersion("1.2019.2.37")]
+[assembly: AssemblyVersion("1.2019.2.44")]
+[assembly: AssemblyFileVersion("1.2019.2.44")]
